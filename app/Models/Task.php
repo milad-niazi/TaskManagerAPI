5698aac2236d7a70be4187cb1656368f93cf6ccd
Milad Niazi <RealMiladNiazi@gmail.com>
2025-11-27 23:44:01 +0330

    Add Task model and migration

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
 }
