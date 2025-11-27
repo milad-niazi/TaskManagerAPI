cdb2bf51deda9e09e77847d4dc8ac18a17d701db
Milad Niazi <RealMiladNiazi@gmail.com>
2025-11-27 23:46:13 +0330

    Add TaskRepository and TaskRepositoryInterface

--- app/Repositories/Interfaces/TaskRepositoryInterface.php
+++ app/Repositories/Interfaces/TaskRepositoryInterface.php
@@ -2,7 +2,13 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Models\Task;
+
 interface TaskRepositoryInterface
 {
-    //
+    public function all();
+    public function find(int $id): ?Task;
+    public function create(array $data): Task;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
 }
--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -2,9 +2,37 @@
 
 namespace App\Repositories;
 
+use App\Models\Task;
 use App\Repositories\Interfaces\TaskRepositoryInterface;
 
 class TaskRepository implements TaskRepositoryInterface
 {
-    //
+    public function all()
+    {
+        return Task::all();
+    }
+
+    public function find(int $id): ?Task
+    {
+        return Task::find($id);
+    }
+
+    public function create(array $data): Task
+    {
+        return Task::create($data);
+    }
+
+    public function update(int $id, array $data): bool
+    {
+        $task = $this->find($id);
+        if (!$task) return false;
+        return $task->update($data);
+    }
+
+    public function delete(int $id): bool
+    {
+        $task = $this->find($id);
+        if (!$task) return false;
+        return $task->delete();
+    }
 }
